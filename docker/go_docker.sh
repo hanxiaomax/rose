68c474a9d5c4639a9533ba053e92826f07157868
hanxiaomax@qq.com <hanxiaomax@qq.com>
2025-02-03 11:00:03 +0000

    fix go docker

--- docker/go_docker.sh
+++ docker/go_docker.sh
@@ -3,6 +3,5 @@ CURRENT_DIR=$(dirname "$(pwd)")
 docker run -it --rm \
     -v "$CURRENT_DIR":/workspace \
     -w /workspace \
-    -p 127.0.0.1:8000:8000 \
     $(env | grep -E '^(ROS_|PYTHON)' | sed 's/^/-e /') \
     rose:latest
