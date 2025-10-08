e0d101d202758a467bef7465893c5f7a4b98e633
hanxiaomax@qq.com <hanxiaomax@qq.com>
2025-10-08 08:03:04 +0000

    remove interactive

--- roseApp/interactive/core/__init__.py
+++
@@ -1,8 +0,0 @@
-"""
-Interactive CLI core infrastructure
-"""
-
-from .command_router import CommandRouter
-from .interactive_runner import InteractiveRunner
-
-__all__ = ['CommandRouter', 'InteractiveRunner']
